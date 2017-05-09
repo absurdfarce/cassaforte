a8fee4bf19f2448de7980bd01a644f16bae101ec
Bret McGuire <bret.mcguire@datastax.com>
2017-05-09 10:08:44 -0500

    Removing version bump... we aren't deploying a new artifact for this.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.datastax.opscenter/cassaforte "2.0.1-30drivers-1.0.16"
+(defproject com.datastax.opscenter/cassaforte "2.0.1-30drivers-1.0.15"
   :min-lein-version "2.5.0"
   :description "A Clojure client for Apache Cassandra"
   :url "http://clojurecassandra.info"
